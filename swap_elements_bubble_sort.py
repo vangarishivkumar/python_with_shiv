21cc8c15d46ccf94972db07f0d083d2c492e2d9b
SHIVKUMAR VANGARI <vangarishivkumar@gmail.com>
2026-06-29 09:26:12 +0530

    Create swap_elements_bubble_sort.py

---
+++ swap_elements_bubble_sort.py
@@ -0,0 +1,10 @@
+my_list = [8, 10, 6, 2, 4]  # list to sort
+sw = True
+while sw:
+    sw = False
+    for i in range(len(my_list) - 1):  # we need (5 - 1) comparisons
+        if my_list[i] > my_list[i + 1]:  # compare adjacent elements
+            sw= True
+            my_list[i], my_list[i + 1] = my_list[i + 1], my_list[i]  # If we end up here, we have to swap the elements.
+
+print(my_list)
